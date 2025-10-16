fb0703928faa6640faa48fb325824dbe5f2fec81
Plinio Barbosa da Silva <plinio.barbosa@fieb.org.br>
2025-10-16 17:09:54 -0300

    aDDESLMC DI

--- main/main.c
+++ main/main.c
@@ -11,7 +11,36 @@
 #include "lwip/err.h"
 #include "lwip/sys.h"
 #include "WIFI_Manager.h"
-//#include "sdkconfig.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_timer.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_ops.h"
+#include "driver/gpio.h"
+#include "driver/spi_master.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "lvgl.h"
+
+#define LCD_NUM_NC -1
+#define LCD_PIN_NUM_MOSI 19
+#define LCD_PIN_NUM_MISO -1
+#define LCD_PIN_NUM_SCLK 18
+#define LCD_PIN_NUM_CS 5
+#define LCD_PIN_NUM_DC 16
+#define LCD_PIN_NUM_BL 4
+#define LCD_PIN_NUM_RST 23
+
+#define LCD_PIXEL_CLOCK_HZ (20000000) // 20 MHz
+#define LCD_CMD_BITS 8
+#define LCD_PARAM_BITS 8
+
+#define LCD_H_RES 135
+#define LCD_V_RES 240
+#define LCD_SPI_HOST SPI2_HOST
+
 #define LOG_LOCAL_LEVEL ESP_LOG_ERROR
 
 static const char *TAG_MAIN = "WIFI_STATION";
@@ -21,6 +50,8 @@ void app_main(void)
     time_t now;
     char strftime_buf[64];
     struct tm timeinfo;
+
+
 
     //Initialize NVS
     esp_err_t ret = nvs_flash_init();
